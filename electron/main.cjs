780e7ba9bb130ad98050a0d2e8a5b37a96de894a
Dhruv1249 <dhr1249.lm@gmail.com>
2026-02-09 13:05:43 +0530

    Edited some configuration for electron

--- electron/main.cjs
+++ electron/main.cjs
@@ -21,7 +21,8 @@ function createWindow() {
     mainWindow.loadURL("http://localhost:5173");
     mainWindow.webContents.openDevTools();
   } else {
-    mainWindow.loadFile(path.join(__dirname, "../dist/index.html"));
+    // Use app.getAppPath() for correct path resolution in packaged app
+    mainWindow.loadFile(path.join(app.getAppPath(), "dist/index.html"));
   }
 
   mainWindow.on("closed", () => {
